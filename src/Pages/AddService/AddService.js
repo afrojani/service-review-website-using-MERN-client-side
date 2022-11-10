3b4518dabd0f81232ab5aaa2ce7d805fc4a90a21
afrojani <sultana.ani2019@gmail.com>
2022-11-10 11:19:36 -0600

    add service page edited

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,10 +1,67 @@
 import React from 'react';
+// import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const AddService = () => {
     document.title = "Add Service";
+
+    // const { user } = useContext(AuthContext);
+
+    const handleAddService = event => {
+        event.preventDefault();
+        const form = event.target;
+        const title = form.title.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const img = form.img.value;
+
+
+        const service = {
+            img,
+            title,
+            description,
+            price,
+
+        }
+
+
+        fetch('http://localhost:5000/addservice', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(service)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    alert('service added successfully')
+                    form.reset();
+
+                }
+            })
+            .catch(error => console.error(error));
+
+
+    }
+
     return (
-        <div>
-            <p>add a new service</p>
+        <div className='mt-6 bg-slate-200 rounded-xl'>
+            <form onSubmit={handleAddService}>
+
+
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                    <input name="title" type="text" placeholder="Title" className="input input-ghost w-full  input-bordered" required />
+                    <input name="price" type="text" placeholder="Prices" className="input input-ghost w-full  input-bordered" required />
+
+
+                    <input name="description" type="text" placeholder="Description" className="input input-ghost w-full  input-bordered" />
+                    <input name="img" type="text" placeholder="Your photo URL" className="input input-ghost w-full  input-bordered" />
+                </div>
+
+                <input className='btn' type="submit" value="Submit your review" />
+            </form>
+
         </div>
     );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -88,14 +88,14 @@ const ServiceDetail = () => {
                             </thead>
                             <tbody>
                                 {
-                                    menu.map(item => <div key={item.name}>
-                                        <tr>
+                                    menu.map(item =>
+                                        <tr key={item.name}>
 
                                             <td>{item.name}</td>
 
                                             <td>{item.price}</td>
                                         </tr>
-                                    </div>)
+                                    )
                                 }
 
                             </tbody>
@@ -119,15 +119,15 @@ const ServiceDetail = () => {
                     </thead>
                     <tbody>
                         {
-                            reviews.map(review => <div key={review._id}>
-                                <tr>
+                            reviews.map(review =>
+                                <tr key={review._id}>
 
                                     <td><img className='rounded-full w-8 h-8' src={review.photoURL} alt="" /></td>
 
                                     <td>{review.customer}</td>
                                     <td>{review.message}</td>
                                 </tr>
-                            </div>)
+                            )
                         }
 
                     </tbody>
